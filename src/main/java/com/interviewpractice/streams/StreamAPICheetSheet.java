e6f677701982cf6604198056e23d19c6a5e0000d
someshwarreddy8 <somudasari8@gmail.com>
2025-06-23 09:29:28 +0530

    reversed/reverseOrder

--- src/main/java/com/interviewpractice/streams/StreamAPICheetSheet.java
+++ src/main/java/com/interviewpractice/streams/StreamAPICheetSheet.java
@@ -1,11 +1,22 @@
 package com.interviewpractice.streams;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class StreamAPICheetSheet {
     public static void main(String[] args) {
         List<Employee> allEmployees = EmployeeDataBase.getAllEmployees();
 
-        allEmployees.forEach(System.out::println);
+//        allEmployees.forEach(System.out::println);
+
+//        allEmployees.stream().forEach(System.out::println);
+
+//        allEmployees.stream().filter(t -> t.getDept().equals("Development"))
+//                .sorted(Comparator.comparing(Employee::getSalary).reversed())
+//                .forEach(System.out::println);
+
+        List<Integer> list = Arrays.asList(4, 3, 6, 9);
+        list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
     }
 }
